0aa688282f5db49e94a840f8fd27a6432a14e765
ysdyt <freedomandfavor@gmail.com>
2015-02-07 20:12:21 +0900

    changed factory and user

--- koninstagram/db/schema.rb
+++ koninstagram/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207093215) do
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer  "factory_id"
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 20150207093215) do
     t.integer  "customize_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "factories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- koninstagram/db/seeds.rb
+++ koninstagram/db/seeds.rb
@@ -1,3 +1,5 @@
+#coding:utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Factory.create(:name => '松江',:address =>'島根県松江市八雲町東岩坂1754',:phone => '0852-54-1745')
+Factory.create(:name => '浜田',:address =>'島根県浜田市三隅町古市場957-4',:phone => '0855-32-4170')
+
+User.create(:name => '松江太郎')
+User.create(:name => '松江二郎')
+User.create(:name => '松江花小')
+User.create(:name => '松江三郎')
+User.create(:name => '松江健太')
+User.create(:name => '松江小次郎')
